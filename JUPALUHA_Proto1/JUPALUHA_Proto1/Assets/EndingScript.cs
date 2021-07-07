80cb1b229feea80fde4cc0aba5acb368ab572e43
Jhannes <s0577182@htw-berlin.de>
2021-07-07 17:31:38 +0200

    [edited] Probeszene + Endingscript + esgibt viel zu viele bugs

--- JUPALUHA_Proto1/JUPALUHA_Proto1/Assets/EndingScript.cs
+++ JUPALUHA_Proto1/JUPALUHA_Proto1/Assets/EndingScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class EndingScript : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class EndingScript : MonoBehaviour
 
     public GameObject SecInstruSOUND;
     public GameObject ThirdInstruSOUND;
-    public GameObject FourthInstruSOUND;
+    //public GameObject FourthInstruSOUND;
 
     public GameObject FiveInstruSOUND;
 
@@ -25,7 +26,7 @@ public class EndingScript : MonoBehaviour
 
     AudioSource SecInstru;
     AudioSource ThirdInstru;
-    AudioSource FourthInstru;
+    //AudioSource FourthInstru;
 
     AudioSource FiveInstru;
 
@@ -34,64 +35,78 @@ public class EndingScript : MonoBehaviour
 
     public GameObject DrumTrigger;
 
+    //public float zoomSpeed = 1;
+    //public float targetOrtho;
+    //public float smoothSpeed = 2.0f;
+    //public float minOrtho = 1.0f;
+    //public float maxOrtho = 20.0f;
     //public Rigidbody2D rb;
+
+    public bool isfiveon = false;
 
 
     public void Start()
     {
-        SecInstruSOUND.SetActive(true);
-        ThirdInstruSOUND.SetActive(true);
-        FourthInstruSOUND.SetActive(true);
-        FiveInstruSOUND.SetActive(true);
         notaktivInstru();
 
         DissonanzCloud.gameObject.SetActive(true);
 
         //rb = GetComponent<Rigidbody2D>();
         //GiantSoundmill.gameObject.SoundmillRotationScript.rb.angularVelocity
+        //targetOrtho = Camera.main.orthographicSize;
     }
 
     void notaktivInstru()
     {
         SecInstru = SecInstruSOUND.GetComponent<AudioSource>();
         ThirdInstru = ThirdInstruSOUND.GetComponent<AudioSource>();
-        FourthInstru = FourthInstruSOUND.GetComponent<AudioSource>();
-        FiveInstru = FourthInstruSOUND.GetComponent<AudioSource>();
+        //FourthInstru = FourthInstruSOUND.GetComponent<AudioSource>();
+        FiveInstru = FiveInstruSOUND.GetComponent<AudioSource>();
 
-        SecInstru.volume = 0;
-        ThirdInstru.volume = 0;
-        FourthInstru.volume = 0;
-        FiveInstru.volume = 0;
     }
 
     public void Update()
     {
-        if (InstrumentsAktivatorAScript.musicVolume > 0.5 && SecInstru.volume > 0.5 && ThirdInstru.volume > 0.5 && FourthInstru.volume > 0.5)
+        if (InstrumentsAktivatorAScript.musicVolume > 0 && SecInstru.volume > 0 && ThirdInstru.volume > 0)// && FourthInstru.volume > 0.5)
         {
             AllSoundsLoud = true;
             StartCoroutine(DramaticPause());
 
-            FiveInstru.volume += Time.deltaTime / FadeTime;
-            if (FiveInstru.volume > 1f)
-                FiveInstru.volume = 1;
-
+            
         }
 
         if (AllSoundsLoud == true) // && Soundmill dreht sich 
         {
             DissonanzCloud.gameObject.SetActive(false);
 
-            Time.timeScale = 0f;
+            FiveInstru.volume += Time.deltaTime / FadeTime;
+            if (FiveInstru.volume > 1f)
+                FiveInstru.volume = 1;
+            isfiveon = true;
+
+            Time.timeScale = 0f; //bewegung ausschalten
             Camera.main.gameObject.transform.position = new Vector3(-2.2f, -35.1f, -10);
             StartCoroutine(DramaticPause());
             Camera.main.gameObject.transform.position = new Vector3(-2.2f, 92.043f, -10);
         }
+
+        //float scroll = Input.GetAxis("Mouse ScrollWheel");
+        //if (scroll != 0.0f)
+        //{
+        //    targetOrtho -= scroll * zoomSpeed;
+        //    targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+        //}
+
+        //Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
     }
 
     IEnumerator DramaticPause()
     {
         yield return new WaitForSeconds(3f);
     }
+
+}
+
 
     //macht alle Instrumente an + obere Soundmill -> Pause -> Camera nach unten -> Soundcloud verschwunden -> möglichkeit auf die Trommel zu springen -> Text (winner)
     //    -> durch alle sachen durchspringen (alles ignorieren)
@@ -102,4 +117,6 @@ public class EndingScript : MonoBehaviour
     // steht oben auf der platform -> text erscheint -> t drücken -> kamera zoomt rein -> springt runter + kamera zoomt raus ->
     // fliegt durch alles durch und trifft auf trommel auf -> schwarzer bildschirm + sound + nur color
 
-}
+    //beim fall: kameramovement verschnellern, x-bewegung locken, zoom
+
+// soundwolke weg wenn viertes instrument an -> sound 5 wenn man runterspringt.
